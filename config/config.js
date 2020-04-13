cf3e6f46576aabadf8c3d627e5ad6290796edff2
abdulrehan1729 <abdulrehan1729@gmail.com>
2020-04-13 17:18:36 +0530

    changed configuration for data base and set aws rds as dev and prod database

--- config/config.js
+++ config/config.js
@@ -3,11 +3,15 @@ require("dotenv").config();
 const password = parseInt(process.env.DB_PASSWORD);
 module.exports = {
   development: {
-    username: "postgres",
-    password: password,
-    database: "annodb",
-    host: "127.0.0.1",
+    username: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DATABASE,
+    host: process.env.RDS_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: "Amazon RDS"
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30 },
     operatorsAliases: false
   },
   test: {
@@ -19,11 +23,15 @@ module.exports = {
     operatorsAliases: false
   },
   production: {
-    username: "postgres",
-    password: password,
-    database: "annodb",
-    host: "127.0.0.1",
+    username: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DATABASE,
+    host: process.env.RDS_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: "Amazon RDS"
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30 },
     operatorsAliases: false
   }
 };
