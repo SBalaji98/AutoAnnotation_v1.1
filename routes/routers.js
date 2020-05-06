4f9dfb4990041a0a1a9c418ade55456af0b37771
abdulrehan1729 <abdulrehan1729@gmail.com>
2020-05-06 15:16:46 +0530

    added access control config for different user roles

--- routes/routers.js
+++ routes/routers.js
@@ -7,6 +7,11 @@ const passwordController = require("../controllers/password.controller");
 const jwtAuth = require("../middleware/auth");
 const acl = require("express-acl");
 
+// ACL config and inclusion
+
+acl.config({ baseUrl: "/" });
+acl.config({ decodedObjectName: "user" });
+
 // Sign-up route to create a new user
 router.post(
   "/user",
@@ -28,10 +33,6 @@ router.post(
     userController.create(req, res);
   }
 );
-// To verify if user is authorized
-router.get("/user", jwtAuth.authenticate, (req, res, next) => {
-  userController.IsUserAuthorized(req, res, next);
-});
 
 //Login user
 router.post("/user/login", (req, res, next) => {
@@ -39,47 +40,67 @@ router.post("/user/login", (req, res, next) => {
   userController.userLogin(req, res, next);
 });
 
-//Update controller is not proper will have to have a look before using it
-router.put("/user/update", jwtAuth.authenticate, (req, res, next) => {
-  userController.update(req, res, next);
+// To verify if user is authorized
+router.get("/user", jwtAuth.authenticate, acl.authorize, (req, res, next) => {
+  userController.IsUserAuthorized(req, res, next);
 });
+
+//Update controller is not proper will have to have a look before using it
+router.put(
+  "/user/update",
+  jwtAuth.authenticate,
+  acl.authorize,
+  (req, res, next) => {
+    userController.update(req, res, next);
+  }
+);
 
 // router.put("/update-password", (req, res, next) => {
 //   passwordController.updatePassword(req, res, next);
 // });
 
 // User log-out
-router.post("/user/logout", jwtAuth.authenticate, (req, res, next) => {
-  userController.signOut(req, res, next);
-});
+router.post(
+  "/user/logout",
+  jwtAuth.authenticate,
+  acl.authorize,
+  (req, res, next) => {
+    userController.signOut(req, res, next);
+  }
+);
 
 // Forgot password route
-router.post("/user/forgot-password", (req, res) => {
+router.post("/user/forgot-password", acl.authorize, (req, res) => {
   passwordController.sendForgotPasswordMail(req, res);
 });
 
 // route to send reset forgotten password link
-router.get("/user/reset", (req, res) => {
+router.get("/user/reset", acl.authorize, (req, res) => {
   passwordController.resetPassword(req, res);
 });
 
 // to update user details
-router.put("/user/update-user", (req, res, next) => {
+router.put("/user/update-user", acl.authorize, (req, res, next) => {
   userController.update(req, res, next);
 });
 
 // to update forgotton password
-router.put("/user/forgot-password-update", (req, res) => {
+router.put("/user/forgot-password-update", acl.authorize, (req, res) => {
   passwordController.updateForgottenPassword(req, res);
 });
 
 // to get all users
-router.get("/user/all-users", jwtAuth.authenticate, (req, res) => {
-  userController.getAllUsers(req, res);
-});
+router.get(
+  "/user/all-users",
+  jwtAuth.authenticate,
+  acl.authorize,
+  (req, res) => {
+    userController.getAllUsers(req, res);
+  }
+);
 
 // to get annotations done by one user
-router.get("/annotations", jwtAuth.authenticate, (req, res) => {
+router.get("/annotations", jwtAuth.authenticate, acl.authorize, (req, res) => {
   annotationController.getAnnotationsByUsers(req, res);
 });
 
@@ -87,6 +108,7 @@ router.get("/annotations", jwtAuth.authenticate, (req, res) => {
 router.post(
   "/annotations/update-get-image-data-by-user",
   jwtAuth.authenticate,
+  acl.authorize,
   async (req, res, next) => {
     await annotationController.updateImageData(req, res, next);
   }
@@ -96,28 +118,40 @@ router.post(
 router.get(
   "/annotations/get-image-data-by-user",
   jwtAuth.authenticate,
+  acl.authorize,
   (req, res) => {
     annotationController.getImageDataByUser(req, res);
   }
 );
 
 // to get all the annotations of all the users by admin
-router.get("/admin/all-annotations", jwtAuth.authenticate, (req, res, next) => {
-  annotationController.getAnnotations(req, res, next);
-});
+router.get(
+  "/admin/all-annotations",
+  jwtAuth.authenticate,
+  acl.authorize,
+  (req, res, next) => {
+    annotationController.getAnnotations(req, res, next);
+  }
+);
 
 // to get changed fromat data
 router.get(
   "/annotations/admin/all-annotations-csv",
   jwtAuth.authenticate,
+  acl.authorize,
   (req, res, next) => {
     annotationController.changeFormatToCSVXML(req, res, next);
   }
 );
 
 // upload bulk images data in annotation table
-router.post("/annotations/admin/upload-dl-model-annotations", (req, res) => {
-  annotationController.createBulkAnnotationByDLModel(req, res);
-});
+router.post(
+  "/annotations/admin/upload-dl-model-annotations",
+  jwtAuth.authenticate,
+  acl.authorize,
+  (req, res) => {
+    annotationController.createBulkAnnotationByDLModel(req, res);
+  }
+);
 
 module.exports = router;
