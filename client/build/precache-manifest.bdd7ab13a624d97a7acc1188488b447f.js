61f5f7232be03b3ede95509d66b4cbf131726fc5
abdulrehan1729 <abdulrehan1729@gmail.com>
2020-05-10 15:50:05 +0530

    added babelrc and client build

---
+++ client/build/precache-manifest.bdd7ab13a624d97a7acc1188488b447f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "997836e24953458a1c3c",
+    "url": "/react-image-annotate/static/css/main.5f4b8525.chunk.css"
+  },
+  {
+    "revision": "997836e24953458a1c3c",
+    "url": "/react-image-annotate/static/js/main.5876ee3c.chunk.js"
+  },
+  {
+    "revision": "9aafae87b94098b78bd4",
+    "url": "/react-image-annotate/static/js/runtime~main.3f5c9a4f.js"
+  },
+  {
+    "revision": "bb8e3ba40ae4289d4985",
+    "url": "/react-image-annotate/static/js/2.0e248add.chunk.js"
+  },
+  {
+    "revision": "00d8ec7f3c78ac8ba9ac35b433b252d5",
+    "url": "/react-image-annotate/static/media/logo.00d8ec7f.png"
+  },
+  {
+    "revision": "d753e9842c16be5c2d1949b1e54ed824",
+    "url": "/react-image-annotate/static/media/carback.d753e984.jpg"
+  },
+  {
+    "revision": "dd67416779aa3428dd345995873ba71c",
+    "url": "/react-image-annotate/index.html"
+  }
+];
