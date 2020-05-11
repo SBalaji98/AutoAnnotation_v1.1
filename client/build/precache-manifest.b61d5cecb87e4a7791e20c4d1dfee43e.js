205c247264b20f98c7b4066d53c2d5fe75dfd634
Balajis <balajisel2@gmail.com>
2020-05-11 15:24:53 +0530

    package.json change

---
+++ client/build/precache-manifest.b61d5cecb87e4a7791e20c4d1dfee43e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d88598cf3426b8f4f121",
+    "url": "/static/css/main.f693d392.chunk.css"
+  },
+  {
+    "revision": "d88598cf3426b8f4f121",
+    "url": "/static/js/main.da05a4fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1c3471259d414561b933",
+    "url": "/static/js/2.bf33ff1b.chunk.js"
+  },
+  {
+    "revision": "00d8ec7f3c78ac8ba9ac35b433b252d5",
+    "url": "/static/media/logo.00d8ec7f.png"
+  },
+  {
+    "revision": "d753e9842c16be5c2d1949b1e54ed824",
+    "url": "/static/media/carback.d753e984.jpg"
+  },
+  {
+    "revision": "18c5cec6602c80706bfb6fee7d68e9cb",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-image-annotate/precache-manifest.6c80f418c6805e89a8c7bb1a498ee5b6.js"
+  "/precache-manifest.b61d5cecb87e4a7791e20c4d1dfee43e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-image-annotate/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
