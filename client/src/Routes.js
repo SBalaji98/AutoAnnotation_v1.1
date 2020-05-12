a939240652aa6f61994042b5c0c67d3e0509ea94
abdulrehan1729 <abdulrehan1729@gmail.com>
2020-05-12 08:32:33 +0530

    changes in router file

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 // import Signup from "./components/Authentication/sign-up";
-import ForgotPassword from "./components/Authentication/forgot-password";
-import ResetPassword from "./components/Authentication/ResetPassword";
+import ForgotPassword from "./components/Authentication/forgot-password"
+import ResetPassword from "./components/Authentication/ResetPassword"
 import Signup from "./components/Authentication/signup"
 
 const Routes = () => (
-  <div>
-    <Switch>
+  <Router>
+    <div>
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/forgot-password" component={ForgotPassword} />
       <Route exact path="/reset/:token" component={ResetPassword} />
-    </Switch>
-  </div>
-);
+    </div>
+  </Router>
+)
 
-export default Routes;
+export default Routes
