10714921ebaf4c083fc49b6a16102f6074ffd319
Balajis <balajisel2@gmail.com>
2020-05-15 10:51:36 +0530

    added client build

---
+++ client/build/precache-manifest.c213b0579a3a18cf487b7dfe8519bcb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "591d8e2c94e78f8ccc12",
+    "url": "/static/css/main.717c65f3.chunk.css"
+  },
+  {
+    "revision": "591d8e2c94e78f8ccc12",
+    "url": "/static/js/main.7edcea49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c2047f60c51b8c5e0367",
+    "url": "/static/js/2.bff824a4.chunk.js"
+  },
+  {
+    "revision": "00d8ec7f3c78ac8ba9ac35b433b252d5",
+    "url": "/static/media/logo.00d8ec7f.png"
+  },
+  {
+    "revision": "d753e9842c16be5c2d1949b1e54ed824",
+    "url": "/static/media/carback.d753e984.jpg"
+  },
+  {
+    "revision": "a5dd3b2a1e226ce9369e8b1fee30a990",
+    "url": "/index.html"
+  }
+];
