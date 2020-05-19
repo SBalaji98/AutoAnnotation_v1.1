04f5ed81e53f1c9216b664f09853415bc1f8768a
abdulrehan1729 <abdulrehan1729@gmail.com>
2020-05-19 18:08:42 +0530

    changed db config to use test environment

--- config/config.js
+++ config/config.js
@@ -9,18 +9,22 @@ module.exports = {
     host: process.env.RDS_HOST,
     dialect: "postgres",
     dialectOptions: {
-      ssl: "Amazon RDS"
+      ssl: "Amazon RDS",
     },
     pool: { maxConnections: 5, maxIdleTime: 30 },
-    operatorsAliases: false
+    operatorsAliases: false,
   },
   test: {
-    username: "postgres",
-    password: password,
-    database: "annodb",
-    host: "127.0.0.1",
+    username: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_TEST_DATABASE,
+    host: process.env.RDS_HOST,
     dialect: "postgres",
-    operatorsAliases: false
+    dialectOptions: {
+      ssl: "Amazon RDS",
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30 },
+    operatorsAliases: false,
   },
   production: {
     username: process.env.RDS_USER,
@@ -29,9 +33,9 @@ module.exports = {
     host: process.env.RDS_HOST,
     dialect: "postgres",
     dialectOptions: {
-      ssl: "Amazon RDS"
+      ssl: "Amazon RDS",
     },
     pool: { maxConnections: 5, maxIdleTime: 30 },
-    operatorsAliases: false
-  }
+    operatorsAliases: false,
+  },
 };
